04f05d2b4aa0eec07e296527e93a9af778ef375d
Backiyalakshmi <backiyabala0307@gmail.com>
2023-10-28 21:05:28 +0530

    day3files

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-function App(){
-  return(
-    <div>
-      Hello World!
-    </div>
+import React from 'react';
+import Note from './components/Note.jsx';
+import './styles/App.css';
 
+function App({notes}) {
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul id='list'>
+        {
+          notes.map(note => 
+            <Note key={ note.id } note={ note } />
+          )
+        }
+      </ul>
+
+      <form>
+        <input />
+        <button onClick={() => console.log('save button clicked')}>Save Note</button>
+      </form>
+    </div>
   )
 }
-  
+
 export default App;
